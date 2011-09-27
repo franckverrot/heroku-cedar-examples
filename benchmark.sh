9ca8469623902a697d9786a71218bdf0950544ed
Franck Verrot <franck@verrot.fr>
2011-09-27 08:44:37 +0200

    Add Benchmarking for Java

--- benchmark.sh
+++ benchmark.sh
@@ -9,3 +9,6 @@ ab -n 1000 -c 100 http://ruby-cedar.herokuapp.com/
 
 # Benchmarking Clojure
 ab -n 1000 -c 100 http://clojure-cedar.herokuapp.com/
+
+# Benchmarking Java
+ab -n 1000 -c 100 http://java-cedar.herokuapp.com/
